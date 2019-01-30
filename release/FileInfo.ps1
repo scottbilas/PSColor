3acc523c39c0829fc2a3a2ec5298b44c97c5a2de
Scott Bilas <scottbilas@gmail.com>
2019-01-30 22:21:06 +0100

    Devicons for ll view (l is next)

--- release/FileInfo.ps1
+++ release/FileInfo.ps1
@@ -1,4 +1,4 @@
-# Helper method to write file length in a more human readable format
+﻿# Helper method to write file length in a more human readable format
 function Write-FileLength($length) {
     if ($length -eq $null) {
         return ""
@@ -19,18 +19,28 @@ function Write-Color-LS([string]$color = "white", $file) {
     {
         $length = ''
         $name = $file.name + '\'
+        $icon = $dir_node_exact_matches[$file.name]
+        if (!$icon) { $icon = $dir_node_default }
     }
     else
     {
         $length = Write-FileLength $file.length
         $name = $file.name
+        $icon = $file_node_exact_matches[$name]
+        if (!$icon) {
+            $icon = $file_node_extensions[[io.path]::GetExtension($name) -replace '^\.', '']
+            if (!$icon) {
+                $icon = $file_node_default
+            }
+        }
     }
 
     Write-host -foregroundcolor $color -nonew (
-        "{0} {1} {2,8} {3}" -f
+        "{0} {1} {2,8} {3} {4}" -f
             $file.mode,
             ('{0:dd-MMM-yy} {0:hh:mm}' -f $file.LastWriteTime).ToLower(),
             $length,
+            $icon,
             $name)
 
     if ($file.target -ne $null) {
@@ -47,7 +57,7 @@ function Write-Color-LS([string]$color = "white", $file) {
             $linkpath = "!! " + $linkpath
         }
 
-        write-host -foregroundcolor $global:PSColor.File.Default.Color -nonew ' -> '
+        write-host -foregroundcolor $global:PSColor.File.Default.Color -nonew " $([char]0xfc32) "
         write-host -foregroundcolor $color $linkpath
     }
     else {
@@ -184,6 +194,232 @@ Function Get-ChildItemColorFormatWide($path) {
     If ($nnl) {  # conditionally add an empty line
         Write-Host
     }
+}
+
+# cribbed from https://github.com/alexanderjeurissen/ranger_devicons/blob/master/devicons.py
+
+# note that this file needs to be saved as UTF8-BOM or powershell will fail to parse
+
+$file_node_default = ''
+
+$file_node_extensions = @{
+    '7z'       = '';
+    'ai'       = '';
+    'apk'      = '';
+    'avi'      = '';
+    'bat'      = '';
+    'bmp'      = '';
+    'bz2'      = '';
+    'c'        = '';
+    'c++'      = '';
+    'cab'      = '';
+    'cbr'      = '';
+    'cbz'      = '';
+    'cc'       = '';
+    'clj'      = '';
+    'cljc'     = '';
+    'cljs'     = '';
+    'coffee'   = '';
+    'conf'     = '';
+    'cp'       = '';
+    'cpio'     = '';
+    'cpp'      = '';
+    'css'      = '';
+    'cxx'      = '';
+    'd'        = '';
+    'dart'     = '';
+    'db'       = '';
+    'deb'      = '';
+    'diff'     = '';
+    'dump'     = '';
+    'edn'      = '';
+    'ejs'      = '';
+    'epub'     = '';
+    'erl'      = '';
+    'exe'      = '';
+    'f#'       = '';
+    'fish'     = '';
+    'flac'     = '';
+    'flv'      = '';
+    'fs'       = '';
+    'fsi'      = '';
+    'fsscript' = '';
+    'fsx'      = '';
+    'gem'      = '';
+    'gif'      = '';
+    'go'       = '';
+    'gz'       = '';
+    'gzip'     = '';
+    'hbs'      = '';
+    'hrl'      = '';
+    'hs'       = '';
+    'htm'      = '';
+    'html'     = '';
+    'ico'      = '';
+    'ini'      = '';
+    'java'     = '';
+    'jl'       = '';
+    'jpeg'     = '';
+    'jpg'      = '';
+    'js'       = '';
+    'json'     = '';
+    'jsx'      = '';
+    'less'     = '';
+    'lha'      = '';
+    'lhs'      = '';
+    'log'      = '';
+    'lua'      = '';
+    'lzh'      = '';
+    'lzma'     = '';
+    'markdown' = '';
+    'md'       = '';
+    'mkv'      = '';
+    'ml'       = 'λ';
+    'mli'      = 'λ';
+    'mov'      = '';
+    'mp3'      = '';
+    'mp4'      = '';
+    'mpeg'     = '';
+    'mpg'      = '';
+    'mustache' = '';
+    'ogg'      = '';
+    'pdf'      = '';
+    'php'      = '';
+    'pl'       = '';
+    'pm'       = '';
+    'png'      = '';
+    'psb'      = '';
+    'psd'      = '';
+    'py'       = '';
+    'pyc'      = '';
+    'pyd'      = '';
+    'pyo'      = '';
+    'rar'      = '';
+    'rb'       = '';
+    'rc'       = '';
+    'rlib'     = '';
+    'rpm'      = '';
+    'rs'       = '';
+    'rss'      = '';
+    'scala'    = '';
+    'scss'     = '';
+    'sh'       = '';
+    'slim'     = '';
+    'sln'      = '';
+    'sql'      = '';
+    'styl'     = '';
+    'suo'      = '';
+    't'        = '';
+    'tar'      = '';
+    'tgz'      = '';
+    'ts'       = '';
+    'twig'     = '';
+    'vim'      = '';
+    'vimrc'    = '';
+    'wav'      = '';
+    'webm'     = '';
+    'xml'      = '';
+    'xul'      = '';
+    'xz'       = '';
+    'yml'      = '';
+    'zip'      = '';
+}
+
+$dir_node_default = ''
+
+$dir_node_exact_matches = @{
+# English
+    '.git'                             = '';
+    'Desktop'                          = '';
+    'Documents'                        = '';
+    'Downloads'                        = '';
+    'Dropbox'                          = '';
+    'Music'                            = '';
+    'Pictures'                         = '';
+    'Public'                           = '';
+    'Templates'                        = '';
+    'Videos'                           = '';
+# Spanish
+    'Escritorio'                       = '';
+    'Documentos'                       = '';
+    'Descargas'                        = '';
+    'Música'                           = '';
+    'Imágenes'                         = '';
+    'Público'                          = '';
+    'Plantillas'                       = '';
+    'Vídeos'                           = '';
+# French
+    'Bureau'                           = '';
+    #'Documents'                        = '';
+    'Images'                           = '';
+    'Musique'                          = '';
+    'Publique'                         = '';
+    'Téléchargements'                  = '';
+    'Vidéos'                           = '';
+# Portuguese
+    #'Documentos'                       = '';
+    'Imagens'                          = '';
+    'Modelos'                          = '';
+    #'Música'                           = '';
+    #'Público'                          = '';
+    #'Vídeos'                           = '';
+    'Área de trabalho'                 = '';
+# Italian
+    'Documenti'                        = '';
+    'Immagini'                         = '';
+    'Modelli'                          = '';
+    'Musica'                           = '';
+    'Pubblici'                         = '';
+    'Scaricati'                        = '';
+    'Scrivania'                        = '';
+    'Video'                            = '';
+# German
+    'Bilder'                           = '';
+    'Dokumente'                        = '';
+    'Musik'                            = '';
+    'Schreibtisch'                     = '';
+    'Vorlagen'                         = '';
+    'Öffentlich'                       = '';
+}
+
+$file_node_exact_matches = @{
+    '.Xdefaults'                       = '';
+    '.Xresources'                      = '';
+    '.bashprofile'                     = '';
+    '.bashrc'                          = '';
+    '.dmrc'                            = '';
+    '.ds_store'                        = '';
+    '.fasd'                            = '';
+    '.gitconfig'                       = '';
+    '.gitignore'                       = '';
+    '.jack-settings'                   = '';
+    '.mime.types'                      = '';
+    '.nvidia-settings-rc'              = '';
+    '.pam_environment'                 = '';
+    '.profile'                         = '';
+    '.recently-used'                   = '';
+    '.selected_editor'                 = '';
+    '.vimrc'                           = '';
+    '.xinputrc'                        = '';
+    'config'                           = '';
+    'dropbox'                          = '';
+    #'exact-match-case-sensitive-1.txt' = 'X1';
+    #'exact-match-case-sensitive-2'     = 'X2';
+    'favicon.ico'                      = '';
+    'gruntfile.coffee'                 = '';
+    'gruntfile.js'                     = '';
+    'gruntfile.ls'                     = '';
+    'gulpfile.coffee'                  = '';
+    'gulpfile.js'                      = '';
+    'gulpfile.ls'                      = '';
+    'ini'                              = '';
+    'ledger'                           = '';
+    'license'                          = '';
+    'mimeapps.list'                    = '';
+    'node_modules'                     = '';
+    'procfile'                         = '';
+    'react.jsx'                        = '';
+    'user-dirs.dirs'                   = '';
 }
 
 Export-ModuleMember Get-ChildItemColorFormatWide
